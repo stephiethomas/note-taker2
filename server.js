45cb8e721b7ea7133e6185cef9089b9773241cbd
Stephanie Thomas <stephie.thomas@gmail.com>
2022-01-30 20:58:21 -0600

    setup-server file

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('inquirer');
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+const PORT = process.env.PORT || 3001
+const path = require('path');
+const apiRoutes = require('./routes/apiRoutes/apiRoutes');
+const htmlRoutes = require('./routes/apiRoutes/htmlRoutes');
+
+//Express middleware
+app.use(express.urlencoded({ extended: false}));
+
+//parse incoming JSON data
+app.use(express.json());
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
