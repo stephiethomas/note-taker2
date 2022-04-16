c7653400fe7f9f8cadece8a9140be39c536e7a4c
Stephanie Thomas <stephie.thomas@gmail.com>
2022-04-16 11:07:40 -0500

    routes

--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -1,67 +1,31 @@
-const fs = require('fs');
 const router = require("express").Router();
 const {saveNote} = require ('../../db/db.json');
-const {v4: uuidv4} = require('uuid');
-// const savedNote = ('pushedNotes');
-
-
-
-
-
-//  const saveNote = fs.readFileSync(path.join(__dirname, '../../db/db.json'));
 
 //Retrieve note route
 router.get('/notes', (req, res) => {
-    res.json(saveNote);
+    saveNote
+    .getNotes()
+    .then((notes) => {
+        return res.json(notes);
+    })
+    .catch((err) => res.status(500).json(err));
 });
 
 //Post route
 router.post('/notes', (req, res) => {
-   let savedNote = req.body.id;
-   res.json("savedNote, title, uuid");
-//    return console.log("+newNote.title.uuid")
-   
-    fs.readFileSync("db/db.json", (err, jsonString) => {
-        savedNote = JSON.parse(jsonString);
-        return;
-    }); 
-    
+   saveNote
+   .addNote(req.body)
+   .then((note) => res.json(note))
+   .catch((err) => res.status(500).json(err));
        
-
-   const writeFile = writeNote => {
-       return new Promise((resolve, reject) => {
-           fs.writeFileSync('db/db.json', writeNote, err => {
-               if (err) {
-                   reject(err);
-                   return;
-               }
-               ok: true,
-               message; (savedNote)
-               
-           });
-
-       });
-   };
-
-    // fs.writeFileSync('/db/db.json', JSON.stringify(saveNote));
-    // res.json(saveNote);
 });
-router.get('/notes', function(req,res) {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
-});
-
 //Delete route (array.filter())
 
-router.delete('/notes/:ID', (req, res) => {
-    notes.splice(req.params.ID, 1);
-    updateNote();
-
-    const deleteNote = updateNote => {
-        fs.writeFile('db/db.json', JSON.stringify(notes, '\t'), err => {
-            if (err);
-            return;
-        });
-    };
+router.delete('/notes/:id', (req, res) => {
+    saveNote
+    .deleteNote(req.params.id)
+    .then(() =>res.json({ ok: true}))
+    .catch((err) => res.status(500).json(err)); 
 });
 
 module.exports = router;
--- routes/apiRoutes/htmlRoutes.js
+++ routes/apiRoutes/htmlRoutes.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const router = require('express').Router();
-const fs = require('fs');
 
-router.get('/', (req, res) => {
+
+router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
